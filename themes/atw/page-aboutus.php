0060a5365e70f6850bd69e9054473a5301b364a2
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-26 19:43:22 -0800

    Add styles to heading to match designs

--- themes/atw/page-aboutus.php
+++ themes/atw/page-aboutus.php
@@ -9,7 +9,7 @@ get_header(); ?>
 		<section>
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-			<h2><?php the_title(); ?></h2>
+			<h2><span><?php the_title(); ?></span></h2>
 			<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
