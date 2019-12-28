73ca37caf92c05e4f49fa724c9e70f17ddbb1932
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-28 14:49:25 -0800

    ARemove side bar on index.php, on loop.php add 1/3 column and 2/3 colum to layout. Change h2 for h3. reindent HTML

--- themes/atw/loop.php
+++ themes/atw/loop.php
@@ -11,20 +11,30 @@
 		<?php endif; ?>
 		<!-- /post thumbnail -->
 
-		<!-- post title -->
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<!-- /post title -->
+		<div class="clear">
 
-		<!-- post details -->
-		<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-		<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-		<!-- /post details -->
+			<div class="grid1-3">
 
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+				<!-- post details -->
+				<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+				<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+				<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+				<!-- /post details -->
 
+			</div><!-- .grid1-3-->
+
+			<div class="grid2-3">
+				<!-- post title -->
+				<h3>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+				</h3>
+				<!-- /post title -->		
+
+				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+			</div><!-- .grid2-3-->
+
+		</div><!--.clear-->
+			
 		<?php edit_post_link(); ?>
 
 	</article>
