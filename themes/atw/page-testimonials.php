670aa64fd3a58065a891fa06cea950191a6c826a
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-28 00:20:30 -0800

    Style testimonial-list section, give content a max-width

--- themes/atw/page-testimonials.php
+++ themes/atw/page-testimonials.php
@@ -6,7 +6,7 @@ get_header(); ?>
 
 	<main role="main">
 		<!-- section -->
-		<section>
+		<section class="testimonials-wrapper">
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
