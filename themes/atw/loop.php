24659e8b0dee5eb98c8a294fec2f911f6d25d35e
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-28 15:49:30 -0800

    Indent code to make more readable and compact on loop.phpp. On functions.php change html5wp_excerpt display count from 20 words to 80

--- themes/atw/loop.php
+++ themes/atw/loop.php
@@ -1,45 +1,68 @@
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()): 
+$i = 0;
+while (have_posts()) : the_post(); ?>
+	<!-- If post is in position 0 (is the first displayed) print section with large image size -->
+	<?php if($i == 0) { ?>
+		<!-- article -->
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<!-- post thumbnail -->
+			<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+					<?php the_post_thumbnail('featuredBlog'); // Declare pixel size you need inside the array ?>
+				</a>
+			<?php endif; ?>
+			<!-- /post thumbnail -->
+			<div class="clear">
+				<div class="grid1-3">
+					<!-- post details -->
+					<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+					<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+					<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+					<!-- /post details -->
+				</div><!-- .grid1-3-->
+				<div class="grid2-3">
+					<!-- post title -->
+					<h3>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+					</h3>
+					<!-- /post title -->
+					<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+				</div><!-- .grid2-3-->
+			</div><!--.clear-->			
+			<?php edit_post_link(); ?>
+		</article>
+		<!-- /article -->
 
-		<!-- post thumbnail -->
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail('featuredBlog'); // Declare pixel size you need inside the array ?>
-			</a>
-		<?php endif; ?>
-		<!-- /post thumbnail -->
+	<!-- Otherwise print medium size image and exclude post details as per design -->
+	<?php } else { ?>
+		<!-- article -->
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<div class="clear">
+				<div class="grid1-3">
+					<!-- post thumbnail -->
+					<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+							<?php the_post_thumbnail('medium'); // Declare pixel size you need inside the array ?>
+						</a>
+					<?php endif; ?>
+					<!-- /post thumbnail -->
+				</div><!-- .grid1-3-->
+				<div class="grid2-3">
+					<!-- post title -->
+					<h3>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+					</h3>
+					<!-- /post title -->
+					<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+				</div><!-- .grid2-3-->
+			</div><!--.clear-->			
+			<?php edit_post_link(); ?>
+		</article>
+		<!-- /article -->
+	<?php } ?><!-- End if/else statement -->
 
-		<div class="clear">
-
-			<div class="grid1-3">
-
-				<!-- post details -->
-				<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-				<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-				<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-				<!-- /post details -->
-
-			</div><!-- .grid1-3-->
-
-			<div class="grid2-3">
-				<!-- post title -->
-				<h3>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h3>
-				<!-- /post title -->		
-
-				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-			</div><!-- .grid2-3-->
-
-		</div><!--.clear-->
-			
-		<?php edit_post_link(); ?>
-
-	</article>
-	<!-- /article -->
-
+<?php $i++; ?>
 <?php endwhile; ?>
 
 <?php else: ?>
