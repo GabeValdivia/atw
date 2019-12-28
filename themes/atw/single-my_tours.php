1fe10a84d023023a1599eb211135578f759649a2
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-27 18:23:11 -0800

    Clean up code, remove section that aren't needed. Add new section for tour-information. Add code to pull in ACF fields for travel details

--- themes/atw/single-my_tours.php
+++ themes/atw/single-my_tours.php
@@ -8,38 +8,40 @@
 
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            
+            <!-- post title -->
+            <h2><span><?php the_title(); ?><span></h2>
+            <!-- /post title -->
 
 			<!-- post thumbnail -->
 			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+				
 					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
+				
 			<?php endif; ?>
-			<!-- /post thumbnail -->
+            <!-- /post thumbnail -->
+            
+            <?php 
+                //dates
+                $format = 'd F, Y';
+                $date = strtotime(get_field('departure_date'));
+                $leavingDate = date_i18n($format, $date);
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+                $returnDate = strtotime(get_field('return_date'));
+                $returnDate = date_i18n($format, $returnDate);
+            ?>
 
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
+            <div class="tour-information">
+                <p><strong>Leaving and Retruning Date:</strong> <?php echo $leavingDate . ' - ' . $returnDate ?></p>
+                <p><strong>Location for Departure:</strong><?php the_field('departure_location'); ?></p>
+                <p><strong>Available Seats:</strong> <?php the_field('available_seats'); ?></p>
+                <p><strong>Price:</strong> $<?php the_field('price'); ?></p>
+            </div>
 
-			<?php the_content(); // Dynamic Content ?>
-
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-
+            <?php the_content(); // Dynamic Content ?>
+            
 			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
 
-			<?php comments_template(); ?>
 
 		</article>
 		<!-- /article -->
@@ -62,6 +64,5 @@
 	<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
