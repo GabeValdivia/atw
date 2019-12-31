d07c1dce1431ba429c7efd572b8ea62ef75d1b8e
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-30 22:50:48 -0800

    Change my_tours CPT to true on Has Archive setting to allow Tours to show up on Post Types Order thereby giving client the ability reorder Upcoming Travel posts

--- themes/atw/front-page.php
+++ themes/atw/front-page.php
@@ -1,5 +1,24 @@
 <?php get_header(); ?>
 
+    <section class="upcoming-tours">
+        <h2><span>Upcoming Travel</span></h2>
+        <?php 
+            $args = array(
+                'post_type' => 'my_tours',
+                'orderby' => 'menu_order',
+                'order' => 'ASC',
+                'posts_per_page' => 3
+            );
+        ?>
 
+        <ul>
+            <?php $tours = new WP_Query($args); ?>
+            <?php while($tours->have_posts()): $tours->the_post(); ?>
+
+            <?php the_title(); ?>
+
+            <?php endwhile; wp_reset_postdata(); ?>
+        </ul>
+    </section>
 
 <?php get_footer(); ?>
