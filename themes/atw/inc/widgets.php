449b87df5f8622f9ec5a14c3e9d5252a0b157d09
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-31 19:18:23 -0800

    Write widget code for coupons widget, add widget to footer, test front end to see it displays

--- themes/atw/inc/widgets.php
+++ themes/atw/inc/widgets.php
@@ -137,7 +137,7 @@ class RandmonTravelTips extends WP_Widget {
                 'order' =>'ASC'
             ); ?>
 
-            <ul class="tours list-footer">
+            <ul class="blog-postS">
                 <?php $post = new WP_Query($query); while($post->have_posts()): $post->the_post(); ?>
 
                     <li class="clear">
@@ -199,3 +199,101 @@ function register_random_tips() {
     register_widget( 'RandmonTravelTips' );
 }
 add_action( 'widgets_init', 'register_random_tips' );
+
+
+/**
+ * Adds Coupons Images widget.
+ */
+class CouponsWidget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'coupons_widget', // Base ID
+			__( 'ATW Coupons', 'text_domain' ), // Name
+			array( 'description' => __( 'Show 2 coupons', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+		}
+        
+            $query = array(
+                'post_type' => 'coupons',
+                'posts_per_page' => 2,
+                'orderby' => 'date',
+                'order' =>'DESC'
+            ); ?>
+
+            <ul class="coupns">
+                <?php $coupons = new WP_Query($query); while($coupons->have_posts()): $coupons->the_post(); ?>
+
+                    <li class="clear">
+					
+                        <div class="content-coupon">
+                            <img src="<?php the_field('image'); ?>">
+                        </div> <!-- .content-coupon -->
+
+
+                    </li>
+
+                <?php endwhile; wp_reset_postdata(); ?>
+            </ul>
+        
+		<?php echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label> 
+		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+
+} // class Foo_Widget
+
+// register Foo_Widget widget
+function register_coupons_widget() {
+    register_widget( 'CouponsWidget' );
+}
+add_action( 'widgets_init', 'register_coupons_widget' );
